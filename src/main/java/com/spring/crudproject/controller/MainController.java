a5ec6090c8df10c6f7bb86c1e3f6c7ef1fca8712
namtndus <rnrmf0721@gmail.com>
2025-07-28 22:01:30 +0900

    feat: #2 매개변수를 받아 출력

--- src/main/java/com/spring/crudproject/controller/MainController.java
+++ src/main/java/com/spring/crudproject/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.spring.crudproject.controller;
 
+import com.spring.crudproject.dto.ArticleTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class MainController {
 
     @PostMapping("/project")
-    public String project() {
-        return "project";
+    public ResponseEntity<ArticleTemplate> project(@RequestBody ArticleTemplate article) {
+        return article != null ? ResponseEntity.ok(article) : ResponseEntity.notFound().build();
     }
 }
